b35721185be7b6b99eee5c0309be41b55335b9b5
Replit user <>
2022-10-17 02:52:11 +0000

    new try

--- heap.c
+++ heap.c
@@ -55,13 +55,13 @@ void heap_pop(Heap* pq){
   int prioridad = pq->heapArray[0].priority;
   int current = 1;
 
-  while(((current <= pq->size) && (pq->heapArray[current].priority > prio))||((current <= pq->size) && (pq->heapArray[current+1].priority > prio))){
+  while(((current <= pq->size) && (pq->heapArray[current].priority > prio))||((current <= pq->size) && (pq->heapArray[current+1].priority > prioridad))){
     heapElem aux = pq->heapArray[(current-1)/2];
     if(pq->heapArray[current+1].priority > prioridad) 
       current++;
     pq->heapArray[(current-1)/2] = pq->heapArray[current];
     pq->heapArray[current] = aux;
-    current = 2*current+1
+    current = 2*current+1;
   }
 }
     
