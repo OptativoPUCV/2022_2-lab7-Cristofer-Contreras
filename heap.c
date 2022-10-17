02794c24a0b1f57a3a9af6fb0ff75c7be5fae930
Replit user <>
2022-10-17 02:51:56 +0000

    new try

--- heap.c
+++ heap.c
@@ -50,8 +50,21 @@ void heap_push(Heap* pq, void* data, int priority){
 
 
 void heap_pop(Heap* pq){
+  pq->size--;
+  pq->heapArray[0] = pq->heapArray[pq->size];
+  int prioridad = pq->heapArray[0].priority;
+  int current = 1;
 
+  while(((current <= pq->size) && (pq->heapArray[current].priority > prio))||((current <= pq->size) && (pq->heapArray[current+1].priority > prio))){
+    heapElem aux = pq->heapArray[(current-1)/2];
+    if(pq->heapArray[current+1].priority > prioridad) 
+      current++;
+    pq->heapArray[(current-1)/2] = pq->heapArray[current];
+    pq->heapArray[current] = aux;
+    current = 2*current+1
+  }
 }
+    
 
 Heap* createHeap(){
   Heap* Nheap = (Heap*)malloc(sizeof(Heap));
