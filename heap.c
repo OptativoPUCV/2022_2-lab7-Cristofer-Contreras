6349dfe91fc20e6bd35ba51cdbc09dd873ef5c14
Replit user <>
2022-10-17 00:00:22 +0000

    new try

--- heap.c
+++ heap.c
@@ -26,7 +26,23 @@ void* heap_top(Heap* pq){
 
 
 void heap_push(Heap* pq, void* data, int priority){
+  if(pa->size+1 > pa->capac){
+    pq->capac = pq->capac*2+1;
+    pq->heapArray = (heapElem*)realloc(pq->heapArray,sizeof(heapElem)*pq->capac);
+  }
+  pq->heapArray[pq->size].data = data;
+  pq->heapArray[pq->size].priority = priority;
+  int max = (pq->size-1)/2;
+  int min = pq->size;
+  heapElem* elemAux = (heapElem*)malloc(sizeof(heapElem));
 
+  while(min > 0){
+    if(pq->heapArray[min].priority > pq->heaArray[max].priority){
+      *elemAux = pq->heapArray[max];
+      pq->heapArray[max] = pq->heapArray[min];
+      pa->heapArray[min] = *elemAux;
+    }
+  }
 }
 
 
@@ -35,10 +51,10 @@ void heap_pop(Heap* pq){
 }
 
 Heap* createHeap(){
-  Heap* aux = (Heap*)malloc(sizeof(Heap));
-  aux-> capac = 3;
-  aux->size = 0;
+  Heap* Nheap = (Heap*)malloc(sizeof(Heap));
+  Nheap-> capac = 3;
+  Nheap->size = 0;
 
-  aux->heapArray= (heapElem*)malloc(3*sizeof(heapElem));  
-  return aux;
+  Nheap->heapArray= (heapElem*)malloc(3*sizeof(heapElem));  
+  return Nheap;
 }
